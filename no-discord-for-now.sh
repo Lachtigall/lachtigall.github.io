5a93473b3fc9468528704fb1524ce0e66a38cdde
zoeyglobe <thenovimatrem@protonmail.ch>
2023-10-22 17:07:10 +0000

    Upload New File

---
+++ no-discord-for-now.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# no-discord-for-now.sh
+sleep 0.25s && sleep 0.01s && echo "" && while true; do sleep 0.01s && killall Discord && pkill Discord && sleep 0.01s && sleep 0.25s; done
+
+# EOF
+
